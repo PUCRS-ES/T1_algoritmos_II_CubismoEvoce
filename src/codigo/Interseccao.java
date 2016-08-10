e200d79a0f79e206e4a28a4eb6ea31e9ba70111a
Vinicius Ferreira <vinicius.gubiani@acad.pucrs.br>
2016-08-10 18:32:37 -0300

    - Adicionados novos metodos para tentar resolver o problema

--- src/codigo/Interseccao.java
+++ src/codigo/Interseccao.java
@@ -7,6 +7,86 @@ public class Interseccao {
 		
 	}
 
+	public boolean estaNasQuinas(Retangulo original, Retangulo corte) {
+		if (original.getX2() <= corte.getX1() && original.getY1() >= corte.getY2() || //superior esquerdo 
+		    original.getX2() <= corte.getX1() && original.getY2() <= corte.getY1() || //inferior esquerdo
+		    original.getX1() >= corte.getX2() && original.getY1() >= corte.getY2() || //superior direito
+		    original.getX1() >= corte.getX2() && original.getY2() <= corte.getY1() ||   //inferior direito
+		    
+		    //centro-vertical, à esquerda
+		    original.getX2() <= corte.getX1() && original.getY2() <= corte.getY2() && original.getY1() >= corte.getY1() ||
+		    
+		    //centro-vertical, à direita
+		    original.getX1() >= corte.getX2() && original.getY2() <= corte.getY2() && original.getY1() >= corte.getY1() ||
+		    
+		    //centro-horizontal, acima
+		    original.getX1() >= corte.getX1() && original.getX2() <= corte.getX2() && original.getY1() >= corte.getY2() ||
+		    
+    		//centro-horizontal, abaixo
+		    original.getX1() >= corte.getX1() && original.getX2() <= corte.getX2() && original.getY2() <= corte.getY1()
+		   )
+			return true;
+		else
+			return false;
+	}
+	
+	public ArrayList<Retangulo> cortaEmPartesVerticais(Retangulo original, Retangulo corte) {
+		
+		ArrayList<Retangulo> lista = new ArrayList<Retangulo>();
+		
+		if (original.getY2() > corte.getY2() && original.getY1() < corte.getY1()) { //retangulo original ocupa 3 linhas
+			Retangulo topo = new Retangulo(original.getX1(), corte.getY2(), original.getX2(), original.getY2());
+			Retangulo meio = new Retangulo(original.getX1(), corte.getY1(), original.getX2(), corte.getY2());
+			Retangulo baixo = new Retangulo(original.getX1(), original.getY1(), original.getX2(), corte.getY1());
+			lista.add(topo);
+			lista.add(meio);
+			lista.add(baixo);
+		}
+		else if(original.getY2() > corte.getY2() && original.getY1() >= corte.getY1()) { //retangulo ocupa a linha de cima e a do meio
+			Retangulo topo = new Retangulo(original.getX1(), corte.getY2(), original.getX2(), original.getY2());
+			Retangulo meio = new Retangulo(original.getX1(), original.getY1(), original.getX2(), corte.getY2());
+			lista.add(topo);
+			lista.add(meio);
+		}
+		else { //retangulo ocupa a linha do meio, e a de baixo
+			Retangulo meio = new Retangulo(original.getX1(), corte.getY1(), original.getX2(), original.getY2());
+			Retangulo baixo = new Retangulo(original.getX1(), original.getY1(), original.getX2(), corte.getY1());
+			lista.add(meio);
+			lista.add(baixo);
+		}
+		
+		return lista;
+	}
+	
+	public ArrayList<Retangulo> cortaEmPartesHorizontais(Retangulo original, Retangulo corte) {
+		
+		ArrayList<Retangulo> lista = new ArrayList<Retangulo>();
+		
+		if (original.getX1() < corte.getX1() && original.getX2() > corte.getX2()) { //retangulo ocupa 3 colunas
+			Retangulo esquerda = new Retangulo(original.getX1(), original.getY1(), corte.getX1(), original.getY2());
+			Retangulo centro = new Retangulo(corte.getX1(), original.getY1(), corte.getX2(), original.getY2());
+			Retangulo direita = new Retangulo(corte.getX2(), original.getY1(), original.getX2(), original.getY2());
+			lista.add(esquerda);
+			lista.add(centro);
+			lista.add(direita);
+		}
+		else if (original.getX1() < corte.getX1() && original.getX2() <= corte.getX2()) { //retangulo ocupa a coluna do meio e da esquerda
+			Retangulo esquerda = new Retangulo(original.getX1(), original.getY1(), corte.getX1(), original.getY2());
+			Retangulo centro = new Retangulo(corte.getX1(), original.getY1(), original.getX2(), original.getY2());
+			lista.add(esquerda);
+			lista.add(centro);
+		}
+		else { //retangulo ocupa a coluna do meio e da direita
+			Retangulo centro = new Retangulo(original.getX1(), original.getY1(), corte.getX2(), original.getY2());
+			Retangulo direita = new Retangulo(corte.getX2(), original.getY1(), original.getX2(), original.getY2());
+			lista.add(centro);
+			lista.add(direita);
+		}
+		
+		return lista;
+	}
+	
+	
 	public Retangulo cortaAEsquerda(Retangulo original, Retangulo corte) {
 		if (original.getX1() < corte.getX1() && corte.getX1() <= original.getX2())
 			return new Retangulo(original.getX1(), original.getY1(), corte.getX1(), original.getY2());
