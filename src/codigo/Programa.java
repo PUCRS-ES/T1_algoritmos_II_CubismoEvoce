d42781237e431bf3725c87708d88bd4cab4c8433
Vinicius Ferreira <vinicius.gubiani@acad.pucrs.br>
2016-08-16 14:05:00 -0300

    - Implementada leitura dos arquivos. Ainda nao conferi se os resultados estao corretos

--- src/codigo/Programa.java
+++ src/codigo/Programa.java
@@ -1,5 +1,7 @@
 package codigo;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Deque;
 import java.util.LinkedList;
@@ -7,60 +9,84 @@ import java.util.LinkedList;
 public class Programa {
 
 	public static void main(String[] args) {
-		Retangulo original = new Retangulo(5, 3, 12, 10);
-		ArrayList<Retangulo> muitosCortes = new ArrayList<Retangulo>();
-		muitosCortes.add(new Retangulo(1,  7,  8, 14));
-		muitosCortes.add(new Retangulo(7,  5, 11, 13));
-		muitosCortes.add(new Retangulo(10, 7, 17, 14));
-		muitosCortes.add(new Retangulo(9,  1, 17,  6));
-		
-		Deque<Retangulo> filaCircular = new LinkedList<Retangulo>();
-		filaCircular.add(original);
-		Interseccao interserc = new Interseccao();
-		int area = 0;
-		
-		for (Retangulo corteAtual : muitosCortes) {
-			int tamanho = filaCircular.size();
+		try {
+			Leitor leitor = new Leitor("c50000");
+			String linha = leitor.leProximaLinha();
+			String[] dados = linha.split(" ");
+			Retangulo original = new Retangulo(
+				Integer.parseInt(dados[0]),
+				Integer.parseInt(dados[1]),
+				Integer.parseInt(dados[2]),
+				Integer.parseInt(dados[3])
+			);
+			Deque<Retangulo> filaCircular = new LinkedList<Retangulo>();
+			filaCircular.add(original);
+			Interseccao interserc = new Interseccao();
+			int area = 0;
 			
-			for (int i = 0; i < tamanho; i++) {
-				Retangulo ret = filaCircular.removeFirst();
-				
-				if (interserc.estaNasQuinas(ret, corteAtual) == true)
-					filaCircular.add(ret);
-				else {
-					ArrayList<Retangulo> horizontal = interserc.cortaHorizontalmente(ret, corteAtual);
-					Retangulo baixo = horizontal.get(0);
-					Retangulo meio = horizontal.get(1);
-					Retangulo topo = horizontal.get(2);
+			linha = leitor.leProximaLinha();
+			int quantidadeDeRetangulos = Integer.parseInt(linha);
+			
+			int atual = 0;
+			while (atual < quantidadeDeRetangulos) {
+				linha = leitor.leProximaLinha();
+				dados = linha.split(" ");
+				Retangulo corteAtual = new Retangulo(
+					Integer.parseInt(dados[0]),
+					Integer.parseInt(dados[1]),
+					Integer.parseInt(dados[2]),
+					Integer.parseInt(dados[3])
+				);
+
+				int tamanho = filaCircular.size();
+				for (int i = 0; i < tamanho; i++) {
+					Retangulo ret = filaCircular.removeFirst();
 					
-					if (baixo != null)
-						filaCircular.add(baixo);
-					if (topo != null)
-						filaCircular.add(topo);
-					
-					if (meio != null) {
-						ArrayList<Retangulo> vertical = interserc.cortaVerticalmente(meio, corteAtual);
-						Retangulo esquerda = vertical.get(0);
-						Retangulo centro = vertical.get(1);
-						Retangulo direita = vertical.get(2);
+					if (interserc.estaNasQuinas(ret, corteAtual) == true)
+						filaCircular.add(ret);
+					else {
+						ArrayList<Retangulo> horizontal = interserc.cortaHorizontalmente(ret, corteAtual);
+						Retangulo baixo = horizontal.get(0);
+						Retangulo meio = horizontal.get(1);
+						Retangulo topo = horizontal.get(2);
 						
-						if (esquerda != null)
-							filaCircular.add(esquerda);
-						if (direita != null)
-							filaCircular.add(direita);
+						if (baixo != null)
+							filaCircular.add(baixo);
+						if (topo != null)
+							filaCircular.add(topo);
+						
+						if (meio != null) {
+							ArrayList<Retangulo> vertical = interserc.cortaVerticalmente(meio, corteAtual);
+							Retangulo esquerda = vertical.get(0);
+							Retangulo centro = vertical.get(1);
+							Retangulo direita = vertical.get(2);
+							
+							if (esquerda != null)
+								filaCircular.add(esquerda);
+							if (direita != null)
+								filaCircular.add(direita);
+						}
 					}
 				}
+				
+				area = 0;
+				for (Retangulo aux : filaCircular)
+					area += aux.calculaArea();
+//				try {
+//					Thread.sleep(1000);
+//				} catch (InterruptedException e) {
+//					e.printStackTrace();
+//				}
+				atual++;
+				System.out.println("Retangulo atual: " + atual + " - Área: " + area);
 			}
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 		
-		while (!filaCircular.isEmpty()) {
-			Retangulo atual = filaCircular.removeFirst();
-			area += atual.calculaArea();
-		}
-					
-		
-		
-		area = area;
 	}
 
 }
